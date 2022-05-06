87cb08df9c7a74508254b60efa1657ee9c1d39f5
KoJaco <kojaco.dev@gmail.com>
2022-05-06 14:56:29 +1000

    ci: implement next-pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,15 +7,8 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    {/* <link
-                        rel="preconnect"
-                        href="https://fonts.googleapis.com"
-                    />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap"
-                        rel="stylesheet"
-                    /> */}
+                    {/* recognize manifest */}
+                    <link rel="manifest" href="/manifest.json" />
 
                     <link
                         rel="preconnect"
